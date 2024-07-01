cbeafaa4e3fb47de869c62793ac25977f43064b5
kobir khan <mdkobirk161@gmail.com>
2024-07-01 22:52:36 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php 
+// echo  "hello php "
+
+
+// $a = 20;
+// $b = 20;
+// sum = $x + $b;
+
+// echo "sum:"
+
+
+
+
+// echo "hello word";
+
+
+// $x =30;
+// $y = 20;
+// $sum = $x + $y;
+// echo "the avarage number is :" . $sum. "<br>";
+
+
+
+// $fristNumber = 80;
+// $lastNumber = 20;
+// $sum = $fristNumber +  $lastNumber;
+// echo "the avarag enumber is :" . $sum ."<br>";
+
+
+// function Name($name) {
+//     if ($name == "kobir") {
+//         echo "The name is: " . $name . "<br>";
+//     } else {
+//         echo "The name is not kobir.<br>";
+//     }
+// }
+
+// Name("kobir");
+
+
+
+// function name($kobir) {
+//     $name = "kobir"; 
+
+//     if ($name == "kobir") {
+//         echo "enter name is: " . $kobir  ."<br>";
+//     } else if ($name == "sagor") {
+//         $lofer = "lofar";
+//         echo "enter name is: " . $lofer  ."<br>";
+//     } else {
+//         $first ="akash";
+//         echo "enter name is: " . $first  . "<br>";
+//     }
+// }
+
+// name("akash");
+  
+
+
+
+// function name($kobir) {
+//     if ($kobir >= 80 && $kobir <= 100) {
+//         echo "Enter grade number: A+";
+//     } else if ($kobir >= 70 && $kobir < 80) {
+//         echo "Enter grade number: A";
+//     } else if ($kobir >= 60 && $kobir < 70) {
+//         echo "Enter grade number: A-";
+//     } else if ($kobir >= 50 && $kobir < 60) {
+//         echo "Enter grade number: B";
+//     } else {
+//         echo "Enter grade number: Fail";
+//     }
+// }
+
+// name(99);
+
+
+
+//  function calculate($language) {
+//     $languages = ["html", "java", "css", "php"];
+    
+//     if [(array [$languages]) ]{
+//         echo "Language entered: " . $language;
+//     } else {
+//         echo "no language";
+//     }
+// }
+
+
+// calculate(paiton);
+
+
+
+
+
